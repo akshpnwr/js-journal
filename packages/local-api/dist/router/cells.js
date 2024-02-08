f4fb55b6ce0d0b6e85cd4a026f62d39ef732b3f7
akshpnwr <akashpanwar9976@gmail.com>
2024-02-08 20:14:30 +0530

    Creating the get and post request

--- packages/local-api/dist/router/cells.js
+++ packages/local-api/dist/router/cells.js
@@ -14,10 +14,38 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.createCellsRouter = void 0;
 const express_1 = __importDefault(require("express"));
+const promises_1 = __importDefault(require("fs/promises"));
+const path_1 = __importDefault(require("path"));
 const createCellsRouter = (filename, dir) => {
     const router = express_1.default.Router();
-    router.get("/cells", (req, res) => __awaiter(void 0, void 0, void 0, function* () { }));
-    router.post("/cells", (req, res) => __awaiter(void 0, void 0, void 0, function* () { }));
+    const fullPath = path_1.default.join(filename, dir);
+    const isLocalApiError = (err) => {
+        return typeof err.code === "string";
+    };
+    router.get("/cells", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+        try {
+            const result = yield promises_1.default.readFile(fullPath, {
+                encoding: "utf-8",
+            });
+            res.send(JSON.parse(result));
+        }
+        catch (err) {
+            if (isLocalApiError(err)) {
+                if (err.code === "ENOENT") {
+                    yield promises_1.default.writeFile(fullPath, "[]", "utf-8");
+                    res.send([]);
+                }
+            }
+            else {
+                throw err;
+            }
+        }
+    }));
+    router.post("/cells", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+        const cells = req.body.cells;
+        yield promises_1.default.writeFile(fullPath, JSON.stringify(cells), "utf-8");
+        res.send({ status: "ok" });
+    }));
     return router;
 };
 exports.createCellsRouter = createCellsRouter;
--- packages/local-api/src/router/cells.ts
+++ packages/local-api/src/router/cells.ts
@@ -1,10 +1,50 @@
 import express from "express";
+import fs from "fs/promises";
+import path from "path";
+
+interface Cell {
+  id: string;
+  content: string;
+  type: "text" | "code";
+}
+
+interface LocalApiError {
+  code: string;
+}
 
 export const createCellsRouter = (filename: string, dir: string) => {
   const router = express.Router();
+  const fullPath = path.join(filename, dir);
 
-  router.get("/cells", async (req, res) => {});
-  router.post("/cells", async (req, res) => {});
+  const isLocalApiError = (err: any): err is LocalApiError => {
+    return typeof err.code === "string";
+  };
+
+  router.get("/cells", async (req, res) => {
+    try {
+      const result = await fs.readFile(fullPath, {
+        encoding: "utf-8",
+      });
+
+      res.send(JSON.parse(result));
+    } catch (err) {
+      if (isLocalApiError(err)) {
+        if (err.code === "ENOENT") {
+          await fs.writeFile(fullPath, "[]", "utf-8");
+          res.send([]);
+        }
+      } else {
+        throw err;
+      }
+    }
+  });
+  router.post("/cells", async (req, res) => {
+    const cells = req.body.cells as Cell[];
+
+    await fs.writeFile(fullPath, JSON.stringify(cells), "utf-8");
+
+    res.send({ status: "ok" });
+  });
 
   return router;
 };
